417e1ed164a2109d7e808c039641e849bd3b2bcd
bunabloom <kimrinoffdev@gmail.com>
2024-09-23 14:42:25 +0900

    Initial Commit

---
+++ TCA_Tutorial/TCA_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_TutorialApp.swift
+//  TCA_Tutorial
+//
+//  Created by KimRin on 9/23/24.
+//
+
+import SwiftUI
+import
+@main
+struct TCA_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
